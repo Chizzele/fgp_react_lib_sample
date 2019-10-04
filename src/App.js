f05667eababcdf4f098530601147acd9a0570efb
Anthony <anthony@future-grid.com.au>
2019-10-04 15:57:26 +1000

    exmaple

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css'; 
-import  {RouteWrap, Page, Navigation, Search} from '@chizzele-/fgp_react_lib';
+import  {RouteWrap, Page, DevicePage, Navigation, Search, DeviceWidget} from '@chizzele-/fgp_react_lib';
 import logo from './logo.svg';
 import searchConfig from './configs/searchconfigTA.json';
 import Welcome from './Welcome';
+import icpConfig from './configs/icpWidgetProcessorConfig.json'
 
 
 function App() {
   const navItems = [{ 
       key:"home01",
-      linkTo:"/Home",
+      linkTo:"/",
       fontAwesomeIcon:"home",
       fontAwesomeLib:"fa",
       description: "Home"
@@ -22,23 +23,30 @@ function App() {
       description: "Search"
     },
     {
-      key:"page02",
+      key:"page01",
       linkTo:"/Page",
       fontAwesomeIcon:"map",
       fontAwesomeLib:"fa",
-      description: "Page"
+      description: "HardCode Map"
+    },
+    {
+      key:"nwp01",
+      linkTo:"/Nwp",
+      fontAwesomeIcon:"plug",
+      fontAwesomeLib:"fa",
+      description: "Network Planning"
     }
   ]
   return (
     <div className="App">
       <RouteWrap>
         <Page 
-          path={"/Home"}
+          exact path={"/"}
         >
           <Navigation
             topNavTitle={"Compass - Home"}
-            sideNavLogoPath={logo}
-            currentPage={"/Home"}
+            sideNavLogo={logo}
+            currentPage={"/"}
             isOpen={false}
             items={navItems}
           />
@@ -46,11 +54,11 @@ function App() {
 
         </Page>
         <Page 
-          path={"/Search"}
+          exact path={"/Search"}
         >
           <Navigation
             topNavTitle={"Compass - Search"}
-            sideNavLogoPath={logo}
+            sideNavLogo={logo}
             currentPage={"/Search"}
             isOpen={false}
             items={navItems}
@@ -63,18 +71,58 @@ function App() {
             defaultSearchType="==*?*"
           />
         </Page>
+        <DevicePage
+          path={"/icp/"}
+          baseUrl="https://compass-api.dev.welnet.co.nz/compass/"
+          deviceType={"icp"}
+          deviceTypeTitleCasing={"upper"}
+          extensionNames={["location", "icp_ext", "icp_event_stats_ext"]}
+          isLoadingNavLogo={logo}
+          relationParentNames={["icp_lvcircuit", "icp_transformer"]}
+          relationChildNames={["meter_icp"]}
+          isLoadingNavItems={navItems}
+          isLoadingNavTitle={"Compass - icp - loading"}
+          mapType={"basic"}
+          mapProjection={"EPSG:4326"}
+          hasBreadCrumbs={true}
+          breadCrumbPath={"icp->lv_circuit->transformer->feeder->zone_sub->gxp"}
+        >
+          <Navigation
+            topNavTitle={"Compass - icp"}
+            sideNavLogo={logo}
+            currentPage={""}
+            isOpen={false}
+            items={navItems}
+          />
+          <DeviceWidget
+            dataLink={true}
+            processorConfig={icpConfig}
+          />
+
+        </DevicePage>
         <Page 
           path={"/Page"}
         >
           <Navigation
-            topNavTitle={"Compass - Change this to whatever"}
-            sideNavLogoPath={logo}
+            topNavTitle={"Compass - Map Example - Hardcoded"}
+            sideNavLogo={logo}
             currentPage={"/Page"}
             isOpen={false}
             items={navItems}
           />
+
         </Page>
-          
+        <Page
+          path={"/Nwp"}
+        >
+          <Navigation
+            topNavTitle={"Compass - Network Planning"}
+            sideNavLogo={logo}
+            currentPage={"/Nwp"}
+            isOpen={false}
+            items={navItems}
+          />
+        </Page>
       </RouteWrap>
     </div>
   );
--- src/myTest.js
+++ src/myTest.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import './myfile.css'
 export class MyTest extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            dataLink : true
+        }
+        console.log(this.state, this.props)    
+    }
     render() {
         return (
             <div className={"fgReact_home"}>
-                {this.props.children}
+                Hello world : heres the prop m8 {this.props.myProp}
             </div>
         )
     }
